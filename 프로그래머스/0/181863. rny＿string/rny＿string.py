2b2e83869e4ad8f4e896794ed4594e29130c555b
Seou0912 <151927766+Seou0912@users.noreply.github.com>
2024-03-12 17:00:13 +0900

    [level 0] Title: rny_string, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181863. rny＿string/rny＿string.py
@@ -0,0 +1,6 @@
+def solution(rny_string):
+    return rny_string.replace('m','rn')
+
+# for rny_string, expected in test_cases:
+#     result = solution(rn_string)
+#     print (f"rny_string: {rny_string}, result:{result}, expected:{expected}")
