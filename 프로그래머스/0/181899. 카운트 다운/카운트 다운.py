36c9c3ace8634d436b9d755d16dd5701feefb7d7
Seou0912 <151927766+Seou0912@users.noreply.github.com>
2024-03-17 17:27:38 +0900

    [level 0] Title: 카운트 다운, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/181899. 카운트 다운/카운트 다운.py
+++ 프로그래머스/0/181899. 카운트 다운/카운트 다운.py
@@ -1,3 +1,3 @@
 def solution(start_num, end_num):
-    result = list(range(start_num, end_num - 1, -1))  # start_num부터 end_num까지 감소하는 수를 리스트로 생성
-    return result
+    return list(range(start_num, end_num - 1, -1))  # start_num부터 end_num까지 감소하는 수를 리스트로 생성
+     
